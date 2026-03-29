00b65df176c968890a420e61483cd019b5ce4874
Ramos <andfera201@gmail.com>
2026-03-28 20:08:32 -0500

    chore: add prisma orm and user model

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -3,4 +3,7 @@ import Joi from 'joi'
 export const envValidation = Joi.object({
   ENVIRONMENT: Joi.string().valid('local', 'production').required(),
   PORT: Joi.number().port().required(),
+  DATABASE_URL: Joi.string()
+    .uri({ scheme: ['postgresql', 'postgres'] })
+    .required(),
 })
